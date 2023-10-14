b81efa0a764bcfe44b7e90958684dfdb44c3ac10
WJX2001 <1758807220@qq.com>
2023-10-14 15:57:23 +0800

    feat: JS

--- day2-JS部分/6.Promise.js
+++ day2-JS部分/6.Promise.js
@@ -123,3 +123,51 @@ Promise.MyAllSettledPlus = function (promises) {
     })
   })
 }
+
+// TODO: promise 案例
+let p = new Promise((resolve, reject) => {
+  let num = Math.ceil(Math.random() * 10)
+  if (num <= 5) {
+    resolve(num)
+  } else {
+    reject('数字太大了')
+  }
+})
+
+p.then((data) => console.log(data)).catch((err) => console.error(err))
+
+// TODO: promise.race 应用场景：设置异步请求的超时时间并在超时后执行相应操作
+
+// 创建一个用于模拟异步请求的函数
+function makeAsyncRequest() {
+  return new Promise((resolve, reject) => {
+    // 模拟异步操作
+    setTimeout(() => {
+      // 异步请求成功的地方
+      resolve('请求成功')
+    }, 2000) // 模拟请求在2秒后完成
+  })
+}
+
+// 创建一个Promise 来表示超时
+function createTimeoutPromise(timeout) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('请求超时')
+    }, timeout)
+  })
+}
+
+//* 测试
+const requestPromise = makeAsyncRequest()
+const timeoutPromise = createTimeoutPromise(1500)
+
+// 使用 Promise.race 来等待第一个结果
+Promise.race([requestPromise, timeoutPromise])
+  .then((result) => {
+    // 如果请求成功，这里会被调用
+    console.log(result) // 输出请求成功
+  })
+  .catch((error) => {
+    console.log(error)
+  })
