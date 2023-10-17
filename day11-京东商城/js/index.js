11b0cd8b8c105aac4ecc5fa1e4686607cfd7f7be
WJX2001 <1758807220@qq.com>
2023-10-17 15:36:57 +0800

    feat: 京东商城

---
+++ day11-京东商城/js/index.js
@@ -0,0 +1,105 @@
+// TODO: 实现模糊查询
+let keyword = document.querySelector('.keyword') // 获取输入框
+let searchHelper = document.querySelector('.search-helper') // 获取搜索下拉列表
+
+// 定义数组模拟存储搜索的内容
+let searchArr = [
+  '小米手机',
+  '华为手机',
+  '苹果手机',
+  '小米电视',
+  '小米平板',
+  '苹果12',
+  '苹果13',
+  '苹果手表',
+]
+// 给输入框绑定内容改变事件
+
+keyword.oninput = function () {
+  searchHelper.innerHTML = ''
+  for (let i = 0; i < searchArr.length; i++) {
+    if (searchArr[i].indexOf(keyword.value) != -1) {
+      searchHelper.innerHTML += '<p>' + searchArr[i] + '</p>'
+      searchHelper.style.display = 'block'
+    }
+  }
+}
+
+keyword.onblur = function () {
+  searchHelper.style.display = 'none'
+}
+
+// TODO: 实现轮播图的切换
+let img = document.querySelector('.img')
+let imgArr = ['2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg']
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+let slide = document.querySelector('.slide')
+let lis = document.querySelectorAll('.banner-btn li')
+let count = 0
+
+// 定义函数，用来切换图片的路径
+function cutImg() {
+  img.src = './image/' + imgArr[count]
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].className = ''
+  }
+
+  lis[count].className = 'active'
+}
+// 设置定时器，每隔三秒切换图片
+let timer = setInterval(function () {
+  count++
+
+  if (count > imgArr.length - 1) {
+    count = 0
+  }
+
+  cutImg()
+}, 3000)
+
+// 点击下一张
+
+next.onclick = function () {
+  count++
+  if (count > imgArr.length - 1) {
+    count = 0
+  }
+  cutImg()
+}
+
+// 点击上一张
+prev.onclick = function () {
+  count--
+  if (count < 0) {
+    count = imgArr.length - 1
+  }
+  cutImg()
+}
+
+// 鼠标滑入div 将定时切换图片干掉
+slide.onmouseover = function () {
+  clearInterval(timer)
+}
+
+// 鼠标滑出div，定时器跑起来
+slide.onmouseout = function () {
+  timer = setInterval(function () {
+    count++
+
+    if (count > imgArr.length - 1) {
+      count = 0
+    }
+
+    cutImg()
+  }, 3000)
+}
+
+// 给li 绑定点击事件
+for (let i = 0; i < lis.length; i++) {
+  lis[i].onclick = () => {
+    count = i
+    cutImg()
+  }
+}
