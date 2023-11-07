ec409d72830b2def2d866f898a9e5fec8bcbf3ac
WJX2001 <1758807220@qq.com>
2023-11-07 10:29:44 +0800

    feat: 自定义hooks

--- day18-react相关/react-basic/src/App.js
+++ day18-react相关/react-basic/src/App.js
@@ -1,17 +1,11 @@
 import NewHookIndex from './components/react高阶组件HOC/2.编写高阶组件/2.1强化props/2.1.1混入props'
 import NewClassIndex from './components/react高阶组件HOC/2.编写高阶组件/2.1强化props/2.1.1混入props'
 import NewStateIndex from './components/react高阶组件HOC/2.编写高阶组件/2.1强化props/2.1.2抽离state控制更新'
-import RenderHocHooks from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/1.1动态渲染(hooks版本)'
-import NewRenderHoc1 from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/1.动态渲染'
-import NewRenderHoc from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/1.动态渲染'
-import FenpianRender1 from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/2.1分片渲染(hooks版本)'
-import FenpianRender from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/2.分片渲染'
-import FenPianRender from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.1条件渲染/2.分片渲染'
-import Debounce from './components/react高阶组件HOC/2.编写高阶组件/2.2控制渲染/2.2.2节流渲染/1.基础：节流原理'
-import EventListenerHoc from './components/react高阶组件HOC/2.编写高阶组件/2.3赋能组件/2.3.1事件监控'
+
+import CustomHooks from './components/玩转react-hooks/1.自定义hooks-条件限定'
 
 function App() {
-  return <EventListenerHoc />
+  return <CustomHooks />
 }
 
 export default App
