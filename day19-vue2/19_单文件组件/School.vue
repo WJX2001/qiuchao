76e091e6ac6ab74a94074ed7c8a6ee25914df35e
WJX2001 <1758807220@qq.com>
2023-11-27 18:02:06 +0800

    feat: vue脚手架

---
+++ day19-vue2/19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h1>Hello World</h1>
+    <h2>学校名称：{{ schoolName }}</h2>
+  </div>
+</template>
+
+
+<script>
+// 组件交互相关的代码（数据，方法）
+export default {
+  name: 'School',
+  data () {
+    return {
+      schoolName: '北京邮电大学'
+    }
+  },
+  methods: {
+    showName () {
+      return this.schoolName
+    }
+  }
+}
+
+
+</script>
+
+<style>
+/* 组件的样式 */
+</style>
---
+++ day20-vue2脚手架/vue_test/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">222222222</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+</style>
