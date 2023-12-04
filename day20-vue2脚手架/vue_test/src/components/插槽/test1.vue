e305fb59eb0745b452ef033360fedf29f6f558dd
WJX2001 <1758807220@qq.com>
2023-12-04 16:19:55 +0800

    feat: vue插槽

---
+++ day20-vue2脚手架/vue_test/src/components/插槽/test1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>默认插槽</h1>
+    <slot>你要不写 我就出来了</slot>
+    <hr />
+
+    <h1>具名插槽</h1>
+    <slot name="slot1">我是具名插槽1</slot>
+    <hr />
+
+    <slot name="slot2">我是具名插槽2</slot>
+
+    <h1>作用域插槽</h1>
+    <slot :data="msg">作用域插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test1Comp',
+  data () {
+    return {
+      msg: '这是作用域插槽要传递的数据'
+    }
+  }
+}
+
+</script>
--- day20-vue2脚手架/vue_test/src/components/统一出口文件/EntryComp1.vue
+++ day20-vue2脚手架/vue_test/src/components/统一出口文件/EntryComp1.vue
@@ -2,17 +2,29 @@
   <div>
     <TransitionComp1></TransitionComp1>
     <TransitionComp2></TransitionComp2>
+    <test1Vue>
+      <template slot="slot1">
+        <div>传给插槽1</div>
+      </template>
+
+      <template scope="dataScope">
+        这是子组件通过作用于给我的：{{ dataScope.data }}
+      </template>
+    </test1Vue>
   </div>
 </template>
 
 <script>
+import test1Vue from '../插槽/test1.vue'
 import TransitionComp1 from '../过渡动画/TransitionComp1.vue'
 import TransitionComp2 from '../过渡动画/TransitionComp2.vue'
+
 export default {
   name: 'EntryComp1',
   components: {
     TransitionComp1,
-    TransitionComp2
+    TransitionComp2,
+    test1Vue
   }
 }
 
