f0be91bc6fead6c1d969036b50debeb099884c77
WJX2001 <1758807220@qq.com>
2024-01-22 14:16:10 +0800

    feat: 工程化学习

--- day29-前端工程化/webpack案例/src/heading.js
+++ day29-前端工程化/webpack案例/src/heading.js
@@ -1,9 +1,12 @@
+import './heading.css'
+
 export default () => {
   const element = document.createElement('h2')
+  element.classList.add('heading')
   element.textContent = 'hello world'
   element.addEventListener('click', () => {
     alert('hello webpack')
   })
 
   return element
-}
+} 
--- day29-前端工程化/webpack案例/webpack.config.js
+++ day29-前端工程化/webpack案例/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'none',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'output')
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+
+      }
+    ]
   }
 }
