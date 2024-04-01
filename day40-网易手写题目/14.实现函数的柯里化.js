5f5aa0d7df6d4a8893c8ef7d2951585c4289b83f
WJX2001 <1758807220@qq.com>
2024-04-01 14:25:15 +0800

    feat: 手写题目

--- day40-网易手写题目/14.实现函数的柯里化.js
+++ day40-网易手写题目/14.实现函数的柯里化.js
@@ -1,13 +1,21 @@
 // TODO: 实现函数的柯里化
 
-function currying() {
-
+// 基本结构 传入一个函数 返回一个函数
+function currying (fn, ...args) {
+  return function (...rest) {
+    const allArgs = [...args, ...rest]
+    if (allArgs.length >= fn.length) {
+      return fn.apply(this, allArgs)
+    } else {
+      return currying(fn, ...allArgs)
+    }
+  }
 }
 
 // 测试柯里化函数
-const sum = (a,b,c,d) => a+b+c+d  
+const sum = (a, b, c, d) => a + b + c + d
 
 console.log(currying(sum)(1)(2)(3)(4)) // 10
-console.log(currying(sum,1)(2)(3)(4)) // 10
-console.log(currying(sum,1,2)(3)(4)) // 10
-console.log(currying(sum,1,2)(3,4)) // 10
+console.log(currying(sum, 1)(2)(3)(4)) // 10
+console.log(currying(sum, 1, 2)(3)(4)) // 10
+console.log(currying(sum, 1, 2)(3, 4)) // 10
